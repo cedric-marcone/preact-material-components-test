5951e76cf99593e88ac1ac75c05f5e9b6069908e
Cédric Marcone <cedric.marcone@valraiso.fr>
2017-05-29 09:09:57 +0200

    set preact-material-components to upstream

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,9 +1,11 @@
 import { h, Component } from 'preact'
-import style from './style'
 
-import {Select} from 'preact-material-components'
+import { Select, Textfield, Formfield } from 'preact-material-components'
 import 'preact-material-components/Menu/style.css';
 import 'preact-material-components/Select/style.css';
+import 'preact-material-components/Textfield/style.css';
+import 'preact-material-components/Formfield/style.css';
+import style from './style'
 
 const generate = () => {
   const xs = []; let i = 0
@@ -12,51 +14,71 @@ const generate = () => {
 }
 
 export default class Home extends Component {
- state = {
-   value: 200,
-   values: generate()
- }
+  state = {
+    value: 200,
+    values: generate(),
+    lastname: '',
+    firstname: ''
+  }
 
- selectedChanged = () => {
-   const selectedIndex = this.select.MDComponent.selectedIndex
-   const selectOptions = this.select.MDComponent.selectedOptions
-   const selectedValues = Array.from(selectOptions).map(o => o.value)
-   console.log(selectedIndex, selectedValues, 'c')
-   this.setState({ value: this.indexToValue(selectedIndex) })
- }
+  selectedChanged = (e) => {
+    const selectedIndex = e.selectedIndex
+    const selectOptions = e.selectedOptions
+    const selectedValues = Array.from(selectOptions).map(o => o.value)
+    console.log(selectedValues)
+    this.setState({ value: this.indexToValue(selectedIndex) })
+  }
 
- selectedRef = (select) => {
-   this.select = select
- }
+  valueToIndex = (value) => (
+    this.state.values.indexOf(value)
+  )
 
- valueToIndex = (value) => (
-   this.state.values.indexOf(value)
- )
-
- indexToValue = (index) => (
-   this.state.values[index]
- )
+  indexToValue = (index) => (
+    this.state.values[index]
+  )
 
   render() {
+    const fieldStyle = { width: '100%' }
+    const inputStyle = { display: 'flex' }
+
     return (
       <div class={style.home}>
         <h1>Select test</h1>
-        <div>
-          <Select style={{width: '100%'}}
-            hintText="Select an option"
-            ref={this.selectedRef}
-            onChange={this.selectedChanged}
-            selectedIndex={this.valueToIndex(this.state.value)}
-          >
-            {
-              this.state.values.map(v =>
-                v > 0
-                  ? <Select.Item value={v}>{`option ${v}`}</Select.Item>
-                  : <Select.Item />
-              )
-            }
-          </Select>
-        </div>
+        <form>
+          <Formfield className={style.customerField}>
+            <Textfield
+              className={style.fullWidth}
+              label="Nom"
+              hintText="Entrez votre nom"
+              value={this.state.lastname}
+              required
+            />
+          </Formfield>
+          <Formfield className={style.customerField}>
+            <Textfield
+              className={style.fullWidth}
+              label="Prénom"
+              value={this.state.firstname}
+              required
+            />
+          </Formfield>
+          <Formfield className={style.customerField}>
+            <Select
+              className={style.fullWidth}
+              hintText="Select an option"
+              onChange={this.selectedChanged}
+              selectedIndex={this.valueToIndex(this.state.value)}
+            >
+              {
+                this.state.values.map(v =>
+                  v > 0
+                    ? <Select.Item value={v}>{`option ${v}`}</Select.Item>
+                    : <Select.Item />
+                )
+              }
+            </Select>
+          </Formfield>
+        </form>
       </div>
     )
   }
